c53ddcb5c35582623fa310e7c04441674a25d8aa
MikolajUz <muzialko@gmail.com>
2024-01-29 15:22:02 +0100

    button for showing scrollbar hidden when not needed

--- src/app/main/components/features/table/table.component.ts
+++ src/app/main/components/features/table/table.component.ts
@@ -1,4 +1,13 @@
-import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  OnDestroy,
+  AfterViewInit,
+  ElementRef,
+  ChangeDetectorRef,
+  HostListener,
+} from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 import { PageEvent } from '@angular/material/paginator';
 
@@ -9,7 +18,7 @@ type T = any;
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss'],
 })
-export class TableComponent implements OnInit, OnDestroy {
+export class TableComponent implements OnInit, OnDestroy, AfterViewInit {
   @Input() tableData$!: Observable<T[]>;
   @Input() pageSize: number = 5;
   @Input() displayedColumns$!: Observable<string[]>;
@@ -17,36 +26,51 @@ export class TableComponent implements OnInit, OnDestroy {
 
   lowValue = 0;
   highValue: number = this.pageSize;
+  scrollbar = false;
+  isScrollbarVisible = false;
 
   private destroy$ = new Subject<void>();
 
-
-  constructor() {}
-
-  ngOnInit(): void {
-
+  constructor(private el: ElementRef, private cdr: ChangeDetectorRef) {}
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.checkScrollbarVisibility();
+      this.cdr.detectChanges();
+    }, 0);
   }
+
+  ngOnInit(): void {}
 
   ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
   }
 
+  private checkScrollbarVisibility() {
+    const container = this.el.nativeElement.querySelector('#table-container');
+    const table = this.el.nativeElement.querySelector('table');
 
+    if (container && table) {
+      const containerWidth = container.clientWidth;
+      const tableWidth = table.clientWidth;
 
-  scrollbar = false;
+      this.isScrollbarVisible = tableWidth > containerWidth;
+    }
+  }
 
   toggleScrollbar() {
     this.scrollbar = !this.scrollbar;
-    console.log('scrollbar', this.scrollbar);
+    this.checkScrollbarVisibility();
   }
-
-
-
 
   handlePageEvent(event: PageEvent) {
     this.pageSize = event.pageSize;
     this.lowValue = event.pageIndex * event.pageSize;
     this.highValue = this.lowValue + event.pageSize;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    this.checkScrollbarVisibility();
   }
 }
