8b93e521616afab40d2ec46b305f27fde38e7603
MikolajUz <muzialko@gmail.com>
2024-02-10 15:38:50 +0100

    auth service, auth token as HTPP-only cookie

--- src/app/main/services/auth.service.ts
+++ src/app/main/services/auth.service.ts
@@ -7,7 +7,6 @@ import { Observable, tap } from 'rxjs';
 })
 export class AuthService {
   private apiUrl = 'your_backend_api_url';
-  private tokenKey = 'auth_token';
 
   constructor(private http: HttpClient) {}
 
@@ -21,34 +20,46 @@ export class AuthService {
     return this.http.post(loginUrl, loginData);
   }
 
-  authenticateUser(credentials: { email: string; password: string }): Observable<any> {
+  authenticateUser(credentials: {
+    email: string;
+    password: string;
+  }): Observable<any> {
     const loginUrl = `${this.apiUrl}/login`;
 
-    return this.http.post(loginUrl, credentials).pipe(
-      tap((response: any) => {
-        // Store JWT token in localStorage
-        localStorage.setItem(this.tokenKey, response.token);
+    return this.http.post(loginUrl, credentials, { observe: 'response' }).pipe(
+      tap((response) => {
+        const authToken = response.headers.get('Authorization');
+        if (authToken) {
+          document.cookie = `auth_token=${authToken}; HttpOnly`;
+        }
       })
     );
   }
 
-  // Method to include JWT token in HTTP headers
   getAuthHeaders(): HttpHeaders {
-    const authToken = localStorage.getItem(this.tokenKey);
+    const authToken = this.getCookie('auth_token');
     return new HttpHeaders().set('Authorization', `Bearer ${authToken}`);
   }
 
-
-
+  private getCookie(name: string): string {
+    const match = document.cookie.match(
+      new RegExp('(^| )' + name + '=([^;]+)')
+    );
+    if (match) return match[2];
+    return '';
+  }
 
   changePassword(passwordData: any): Observable<any> {
     const changePasswordUrl = `${this.apiUrl}/change-password`;
-    return this.http.post(changePasswordUrl, passwordData);
+    return this.http.post(changePasswordUrl, passwordData, {
+      headers: this.getAuthHeaders(),
+    });
   }
-
   resetPassword(resetPasswordData: any): Observable<any> {
     const resetPasswordUrl = `${this.apiUrl}/reset-password`;
-    return this.http.post(resetPasswordUrl, resetPasswordData);
+    return this.http.post(resetPasswordUrl, resetPasswordData, {
+      headers: this.getAuthHeaders(),
+    });
   }
   login(credentials: { email: string; password: string }): Observable<any> {
     const loginUrl = `${this.apiUrl}/login`;
@@ -66,13 +77,17 @@ export class AuthService {
     const checkPasswordUrl = `${this.apiUrl}/check-password`;
     const payload = { currentPassword };
 
-    return this.http.post(checkPasswordUrl, payload);
+    return this.http.post(checkPasswordUrl, payload, {
+      headers: this.getAuthHeaders(),
+    });
   }
 
   updatePassword(newPassword: string): Observable<any> {
     const updatePasswordUrl = `${this.apiUrl}/update-password`;
     const payload = { newPassword };
 
-    return this.http.post(updatePasswordUrl, payload);
+    return this.http.post(updatePasswordUrl, payload, {
+      headers: this.getAuthHeaders(),
+    });
   }
 }
